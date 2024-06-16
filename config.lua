70a2f644a1a1ab6b580c4c6aa04a5d3ba7c9d7cb
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-06-16 09:02:59 +0700

    Move All Files And Delete Old One

---
+++ config.lua
@@ -0,0 +1,43 @@
+Config = {}
+Config.TextState = false
+Config.CurrentLabel = nil
+ShownNoti = false
+
+function ShowText(label, state, options)
+    if state == TextState and label == CurrentLabel then
+        return
+    end
+    TextState = state
+    CurrentLabel = label
+    if state then
+        lib.showTextUI(label)
+        if not ShownNoti and #options > 1 then
+            local text = 'USE SCROLL-WHEEL FOR MORE OPTIONS'
+            local time = 4000
+            local icon = 'arrow-down'
+            lib.notify({
+                id = 'interaction',
+                title = 'Envi-Interact',
+                description = text,
+                position = 'top-right',
+                duration = time,
+                style = {
+                    backgroundColor = '#141517',
+                    color = '#C1C2C5',
+                    ['.description'] = {
+                      color = '#909296'
+                    }
+                },
+                icon = icon,
+                iconColor = '#A020F0'
+            })
+            ShownNoti = true
+        end
+    else
+        lib.hideTextUI()
+        ShownNoti = false
+        CurrentLabel = nil
+    end
+end
+
+
---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+
+author 'Envi-Scripts'
+description 'Interaction Menus'
+version '1.1.0 - Release'
+
+game 'gta5'
+lua54 'yes'
+ 
+client_scripts {
+	'client/*.lua',
+}
+ 
+shared_scripts {
+	'config.lua',
+	'@ox_lib/init.lua'
+}
+
+files {
+    'ui/**'
+}
+
+ui_page 'ui/index.html'
